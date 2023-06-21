7233367d6e9a4c72ce9570b403695f3df6a2d4ef
Thomas Faria <57811152+ThomasFaria@users.noreply.github.com>
2023-06-21 17:43:02 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+LANGUAGE=$1
 # Clone course repository
 REPO_URL="https://github.com/InseeFrLab/funathon2023_sujet3.git"
 CLONE_DIR="/home/onyxia/work/funathon-sujet3"
@@ -7,8 +7,8 @@ git clone --depth 1 $REPO_URL $CLONE_DIR
 cd $CLONE_DIR
 
 # Install required packages
-pip install -r requirements_Python.txt
+pip install -r requirements_"$LANGUAGE".txt
 
 # Open the relevant notebook when starting Jupyter Lab
 jupyter server --generate-config
-echo "c.LabApp.default_url = '/lab/tree/funathon-sujet3/Sujet_3_Python.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+echo "c.LabApp.default_url = '/lab/tree/funathon-sujet3/Sujet_3_$LANGUAGE.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
