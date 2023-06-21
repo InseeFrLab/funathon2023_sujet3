988fd0f35a213ae4ab695cb830d5289408a3f0b8
Thomas Faria <57811152+ThomasFaria@users.noreply.github.com>
2023-06-21 18:11:20 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,22 @@ CLONE_DIR="/home/onyxia/work/funathon-sujet3"
 git clone --depth 1 $REPO_URL $CLONE_DIR
 cd $CLONE_DIR
 
+#!/bin/bash
+
+# Check if the first parameter is "R"
+if [[ $LANGUAGE == "R" ]]; then
+    # Define the path to the package list file
+    package_file="requirements_$LANGUAGE.txt"  # Replace with the path to your package list file
+
+    # Read the package names from the file and install them
+    while read -r package; do
+        Rscript -e "if (!requireNamespace('$package', quietly = TRUE)) install.packages('$package')"
+    done < "$package_file"
+else
+    pip install -r requirements_"$LANGUAGE".txt
+fi
+
 # Install required packages
-pip install -r requirements_"$LANGUAGE".txt
 
 # Open the relevant notebook when starting Jupyter Lab
 jupyter server --generate-config
